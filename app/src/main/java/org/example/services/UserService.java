0950a1c4f0abfcaf7c237270b5d70d49ff4b3b0c
Aman-sys-ui <amanak52141@gmail.com>
2025-07-25 19:20:57 +0530

    Updated all project files

--- app/src/main/java/org/example/services/UserService.java
+++ app/src/main/java/org/example/services/UserService.java
@@ -4,63 +4,59 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import org.example.entities.User;
 import org.example.util.UserServiceUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.*;
 
+import java.util.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.stream.Collectors;
 
 public class UserService {
-    private User user ;
     private List<User> userList;
-    private ObjectMapper objectMapper ;
+    private ObjectMapper objectMapper;
     private static final String USER_FILE_PATH = "app/src/main/java/org/example/localDb/users.json";
 
-    public UserService() throws IOException{
-            loadUserFile();
+    public UserService() throws IOException {
+        loadUserFile();
     }
-
-//    public UserService(User user) throws IOException{
-//        this.user = user;
-//
-//            loadUserFile();
-//
-//    }
 
     public void loadUserFile() throws IOException {
         objectMapper = new ObjectMapper();
         File file = new File(USER_FILE_PATH);
-        userList = objectMapper.readValue(file, new TypeReference<List<User>>() {});
+        if (file.length() == 0) {
+            userList = new ArrayList<>();
+        } else {
+            userList = objectMapper.readValue(file, new TypeReference<List<User>>() {});
+        }
     }
 
-    public void signUp(User user) throws IOException{
-            saveUserToFile(user);
+    public void signUp(User user) throws IOException {
+        saveUserToFile(user);
     }
 
     private void saveUserToFile(User user) throws IOException {
-        objectMapper =new ObjectMapper();
+        objectMapper = new ObjectMapper();
         userList.add(user);
         File usersFile = new File(USER_FILE_PATH);
         objectMapper.writeValue(usersFile, userList);
-        System.out.println(user.getName() + "added");
+        System.out.println(user.getName() + " added");
     }
 
-    public void login(User LoginUser) throws Exception {
-        Optional<String> foundUser = userList.stream().filter (user -> {
+    public User login(User loginUser) throws Exception {
+        Optional<User> foundUser = userList.stream().filter(user -> {
             try {
-                return user.getUserName().equals(LoginUser.getUserName()) &&
-                        UserServiceUtil.verifyPassword(LoginUser.getPassword(),
-                                user.getPassword());
+                return user.getUserName().equals(loginUser.getUserName()) &&
+                        UserServiceUtil.verifyPassword(loginUser.getPassword(), user.getPassword());
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
-        }).map(User::getName).findFirst();
+        }).findFirst();
 
-        foundUser.ifPresent(e->System.out.println("welcome " + e));
-
+        if (foundUser.isPresent()) {
+            System.out.println("Welcome " + foundUser.get().getName());
+            return foundUser.get();
+        } else {
+            System.out.println("Invalid credentials.");
+            return null;
+        }
     }
-
 }
